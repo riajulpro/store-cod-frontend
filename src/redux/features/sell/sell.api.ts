5d98a6280f05c8c96335d1ad7c7cd07753630f6f
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-07 15:27:55 +0600

    update

--- src/redux/features/sell/sell.api.ts
+++ src/redux/features/sell/sell.api.ts
@@ -5,14 +5,21 @@ const sellAPI = api.injectEndpoints({
   endpoints: (builder) => ({
     getAllSells: builder.query<
       {
-        data: any; sales: ISell[]; total: number 
-},
+        data: any;
+        sales: ISell[];
+        total: number;
+      },
       { page: number; limit: number }
     >({
       query: ({ page, limit }) => `sell?page=${page}&limit=${limit}`,
       providesTags: ["Sell"],
     }),
+    getUserOrderHistroy: builder.query({
+      query: ({ page = 1 }: { page?: number }) =>
+        `/sell/my/orders?page=${page}&limit=${10}`,
+      providesTags: ["Sell"],
+    }),
   }),
 });
 
-export const { useGetAllSellsQuery } = sellAPI;
+export const { useGetAllSellsQuery, useGetUserOrderHistroyQuery } = sellAPI;
--- src/types/sell.ts
+++ src/types/sell.ts
@@ -1,11 +1,10 @@
 import { TCustomer } from "./customer";
 import { IProduct } from "./product";
 
-
 export interface ISell {
-    _id: string;
-    productId: IProduct | string | {name:string};
-    quantity: number;
-    date: string;
-    customer: TCustomer | string | {firstName: string};
-  }
+  _id: string;
+  productId: IProduct | string | { name: string };
+  quantity: number;
+  date: string;
+  customer: TCustomer | string | { firstName: string };
+}
